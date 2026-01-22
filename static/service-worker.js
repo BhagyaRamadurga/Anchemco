a5b4ddf19803f522cbc2eac8385b13f85c82f806
BhagyaRamadurga <Bhagyani2941@gmail.com>
2026-01-22 10:16:11 +0530

    Sharanu app

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,45 @@
+const CACHE_NAME = 'sharanu-v1';
+const ASSETS = [
+    '/',
+    '/static/style.css',
+    '/static/manifest.json',
+    '/static/icon-512.jpeg',
+    '/static/service-worker.js'
+];
+
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(ASSETS);
+        })
+    );
 });
+
 self.addEventListener('fetch', (e) => {
-    // Basic pass-through
-    e.respondWith(fetch(e.request));
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then((response) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
 });
